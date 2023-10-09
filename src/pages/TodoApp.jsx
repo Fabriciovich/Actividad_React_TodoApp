5e4f4f9b144e32abe878deb245c2f677d04245da
Fabriciovich <fabriciocostadoni@gmail.com>
2023-10-09 14:58:49 -0300

    Se modifico la ubicacion del todoapp, se lo centro en el medio de la pagina para mas prolijidad

--- src/pages/TodoApp.jsx
+++ src/pages/TodoApp.jsx
@@ -1,14 +1,14 @@
 import { Title } from "../../src/components/Title.jsx"
 import { AddTodo } from "../../src/components/AddTodo.jsx"
 import { TodoList } from "../../src/components/TodoList.jsx"
-  export const TodoApp = () => {
-    
+export const TodoApp = () => {
 
-    return (
-      <>
-        <Title/>
-        <TodoList/>
-        <AddTodo/>
-        </>
-    )
-  }
+
+  return (
+    <>
+      <Title />
+      <TodoList />
+      <AddTodo />
+    </>
+  )
+}
